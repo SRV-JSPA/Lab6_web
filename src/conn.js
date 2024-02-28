a29a11567ff12e6e227d92c09bab11ccbca39b70
JSPA <per22318@uvg.edu.gt>
2024-02-27 20:52:27 -0600

    cambios

--- src/conn.js
+++ src/conn.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
     host: 'localhost',
-    user: 'blog_user',
-    database: 'blog_db',
-    password: 'blog_password',
+    user: 'pereira',
+    database: 'blog',
+    password: 'blog2024',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
--- src/main.js
+++ src/main.js
@@ -7,6 +7,26 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/posts', (req, res) => {
+  res.send('obteniendo todos los posts')
+})
+
+app.get('/posts/:id', (req, res) => {
+  res.send('obteniendo el post con el id')
+})
+
+app.post('/posts', (req, res) => {
+  res.send('ingresando info')
+})
+
+app.put('/posts/:id', (req, res) => {
+  res.send('actualizando post')
+})
+
+app.delete('/posts/:id', (req, res) => {
+  res.send('eliminando el post')
+})
+
 app.listen(port, () => {
   console.log(`Server listening at http://127.0.0.1:${port}`)
 })

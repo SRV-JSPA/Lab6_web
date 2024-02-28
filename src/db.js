083a8be2fcb32bfe93681126c85267ca0b55bf10
JSPA <per22318@uvg.edu.gt>
2024-02-28 12:44:26 -0600

    cambios archivos db.js y main.js

--- src/db.js
+++ src/db.js
@@ -10,8 +10,19 @@ export async function getPost(id) {
     return rows;
 }
 
+export async function updatePost(id, title, content, created_at, car_name, company) {
+    const [rows] = await conn.query('UPDATE blog_posts SET title = ?, content = ?, created_at = ?, car_name = ?, company = ? WHERE id = ?', [title, content, created_at, car_name, company, id]);
+    return rows;
+}
 
-export async function createPost(title, content) {
-    const [result] = await conn.query('INSERT INTO blog_posts (title, content) VALUES (?, ?)', [title, content])
+export async function deletePost(id) {
+    const [rows] = await conn.query('DELETE FROM blog_posts WHERE id = ?', [id]);
+    return rows;
+}
+
+
+
+export async function createPost(title, content, created_at, car_name, company) {
+    const [result] = await conn.query('INSERT INTO blog_posts (title, content, created_at, car_name, company) VALUES (?, ?, ?, ?, ?)', [title, content, created_at, car_name, company])
     return result
 }
--- src/main.js
+++ src/main.js
@@ -1,35 +1,62 @@
 import express from 'express'
-import { getAllPosts, getPost } from './db.js'
+import { getAllPosts, getPost, createPost, updatePost, deletePost } from './db.js'
+import bodyParser from 'body-parser'
 
 const app = express()
 const port = 3000
 app.use(express.json())
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
 app.get('/posts', async (req, res) => {
-  const posts = await getAllPosts()
-  res.status(200).json(posts)
+  const posts = await getAllPosts();
+  res.status(200).json(posts);
 })
 
 app.get('/posts/:id', async (req, res) => {
   const postId = req.params.id;
-  const post = await getPost(postId)
-    res.status(200).json(post)
+  const post = await getPost(postId);
+    res.status(200).json(post);
 })
 
-app.post('/posts', (req, res) => {
-  res.send('ingresando info')
+app.post('/posts', async (req, res) => {
+  req.headers['content-type'] === 'application/json';
+  const info = req.body;
+  
+  const titulo = info.title;
+  const contenido = info.content;
+  let ahora = new Date();
+  const creado = ahora.toLocaleTimeString();
+  const carro = info.car;
+  const marca = info.brand;
+
+  const nuevoPost = await createPost(titulo, contenido, creado, carro, marca);
+
+  res.status(200).send("Información enviada con éxito: " + JSON.stringify(info));
+
 })
 
-app.put('/posts/:id', (req, res) => {
-  res.send('actualizando post')
+app.put('/posts/:id', async (req, res) => {
+  const id = req.params.id;
+  const postActual = await getPost(id);
+  const nuevaInfo = req.body; 
+  const titulo = nuevaInfo.title;
+  const contenido = nuevaInfo.content;
+  const creado = postActual.created_at;
+  const carro = nuevaInfo.car;
+  const marca = nuevaInfo.brand;
+  try {
+    const actualizacion = await updatePost(id, titulo, contenido, creado, carro, marca); 
+    res.status(200).json(nuevaInfo); 
+  } catch (error) {
+    res.status(500).json({ message: "Error al actualizar el post" }); 
+  }
 })
 
 app.delete('/posts/:id', (req, res) => {
-  res.send('eliminando el post')
+  const id = req.params.id;
+  deletePost(id);
+  res.status(204)
 })
 
 app.listen(port, () => {

f8e7a7a7a96eeeea1c52d0170a0478f059371ea7
Santiago Pereira <97372983+SRV-JSPA@users.noreply.github.com>
2024-04-26 16:43:56 -0600

    implementacion de tabla usuarios

--- dockerfiles/mysql/schema.sql
+++ dockerfiles/mysql/schema.sql
@@ -8,6 +8,11 @@ CREATE TABLE IF NOT EXISTS blog_posts (
     imagen TEXT NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario VARCHAR(255) NOT NULL,
+    contrasena VARCHAR(255) NOT NULL
+);
 
 
 
--- dockerfiles/mysql/usuarios.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS usuarios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    usuario VARCHAR(255) NOT NULL,
-    contraseña VARCHAR(255) NOT NULL
-);
